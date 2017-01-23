cf958c95ccb6fc94bf2a7b458c7b37f714a79136
raptoria <tina.lacey@gmail.com>
2017-01-23 12:06:44 -0500

    streamlined karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,12 @@
 var argv = require('yargs').argv;
 var path = require('path');
-var combineLoaders = require('webpack-combine-loaders');
+var webpackConfig = require('./webpack.config');
 
 module.exports = function(config) {
   config.set({
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: false,//!argv.watch, // just run once by default
+    concurrency: Infinity,
     frameworks: ['mocha', 'chai'],
     // npm i karma-spec-reporter --save-dev
     // displays tests in a nice readable format
@@ -13,8 +14,6 @@ module.exports = function(config) {
 
     // include some polyfills for babel and phantomjs
     files: [
-      'node_modules/babel-polyfill/dist/polyfill.js',
-      './node_modules/phantomjs-polyfill/bind-polyfill.js',
       './test/**/*.js' // specify files to watch for tests
     ],
     preprocessors: {
@@ -23,52 +22,9 @@ module.exports = function(config) {
       ['./test/**/*.js']: ['webpack', 'sourcemap']
     },
     webpack: {
-       devtool: 'inline-source-map',
-       resolve: {
-        // allow us to import components in tests like:
-        // import Example from 'components/Example';
-        root: path.resolve(__dirname, './src'),
-
-        // allow us to avoid including extension name
-        extensions: ['', '.js', '.jsx', '.json'],
-
-        // required for enzyme to work properly
-        alias: {
-          'sinon': 'sinon/pkg/sinon'
-        }
-      },
-      module: {
-        // don't run babel-loader through the sinon module
-        noParse: [
-          /node_modules\/sinon\//
-        ],
-        // run babel loader for our tests
-        loaders: [
-          {
-            test: /\.(js|jsx)$/,
-            loaders: ['react-hot', 'babel'],
-            exclude: /node_modules/
-          },
-          {
-          test: /\.css$/,
-          loader: combineLoaders([
-            {
-              loader: 'style-loader'
-            }, {
-              loader: 'css-loader',
-              query: {
-                modules: true,
-                localIdentName: '[name]__[local]___[hash:base64:5]'
-              }
-             }
-            ])
-        },
-        { test: /\.(png|jpg|jpeg|gif|svg)$/, loader: "url-loader?limit=100000" },
-        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
-        { test: /\.json$/, loader: "json"}
-        ],
-      },
+      devtool: 'inline-source-map',
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve,
       // required for enzyme to work properly
       externals: {
         'react/addons': true,
@@ -87,7 +43,7 @@ module.exports = function(config) {
       'karma-chrome-launcher',
       'karma-phantomjs-launcher',
       'karma-spec-reporter',
-      'karma-sourcemap-loader'
+      'karma-sourcemap-loader',
     ]
   });
 };
